69e479a261ce51cef23409e17d374cd66bb430d2
VuongBaPhuc <phucvbgcs230123@fpt.edu.vn>
2025-02-15 17:08:48 +0700

    Update PartTimeStudent.java

--- PartTimeStudent.java
+++ PartTimeStudent.java
@@ -1,1 +1,30 @@
 
+public class PartTimeStudent extends Student {
+    private static int count = 0; // Biến đếm tĩnh cho PartTimeStudent
+    private int minHour;
+    private int maxHour;
+
+    public PartTimeStudent() {
+        count++; // Tăng biến đếm khi khởi tạo
+    }
+
+    public PartTimeStudent(String name, String major, String id) {
+        super(name, major, id);
+        count++;
+    }
+
+    public PartTimeStudent(String name, String major, String id, int minHour, int maxHour) {
+        super(name, major, id);
+        this.minHour = minHour;
+        this.maxHour = maxHour;
+        count++;
+    }
+
+    public void registerHour(int hour) {
+        // Cài đặt đăng ký giờ
+    }
+
+    public static int count() {
+        return count; // Trả về số lượng đối tượng PartTimeStudent đã được tạo
+    }
+}
